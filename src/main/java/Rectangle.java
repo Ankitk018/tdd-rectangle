78951316c7dfd59fd11c96c5b40eb10381ea1282
ankitk018 <ankit.kumar1@thoughtworks.com>
2024-04-29 16:34:35 +0530

    [manas/ankit] Refactored the code to accept arguments from constructor

--- src/main/java/Rectangle.java
+++ src/main/java/Rectangle.java
@@ -1,10 +1,19 @@
 public class Rectangle {
 
+    private double length;
+    private double width;
 
-    public double calculateArea(double length, double width) {
+    public Rectangle(double length, double width){
         if(length<0 || width < 0){
             throw new IllegalArgumentException("Length and Width must be non-negative values");
         }
+
+        this.length=length;
+        this.width=width;
+    }
+
+    public double calculateArea() {
+
         return length*width;
     }
 }
--- src/test/java/TestAreaOfRectangle.java
+++ src/test/java/TestAreaOfRectangle.java
@@ -5,45 +5,47 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class TestAreaOfRectangle {
 
-    // both the numbers are positive
-    //one value is negative
-    //both are negative
-    //zeros
     @Test
     public void testAreaPositiveValues() {
-        //assign
-        Rectangle rectangle = new Rectangle();
-        //act //assert
-        assertEquals(20, rectangle.calculateArea(4,5));
+
+        Rectangle rectangle = new Rectangle(4,5);
+
+        assertEquals(20, rectangle.calculateArea());
     }
 
     @Test
     public void testAreaForNegativeLength(){
-        Rectangle rectangle = new Rectangle();
-        assertThrows(IllegalArgumentException.class, () -> rectangle.calculateArea(-4, 5));
+        Rectangle rectangle = new Rectangle(-4, 5);
+        assertThrows(IllegalArgumentException.class, () -> rectangle.calculateArea());
     }
 
     @Test
     public void testAreaForNegativeWidth(){
-        Rectangle rectangle = new Rectangle();
-        assertThrows(IllegalArgumentException.class, () -> rectangle.calculateArea(4, -5));
+        Rectangle rectangle = new Rectangle(4, -5);
+        assertThrows(IllegalArgumentException.class, () -> rectangle.calculateArea());
     }
 
     @Test
     public void testAreaForNegativeLengthAndWidth(){
-        Rectangle rectangle = new Rectangle();
-        assertThrows(IllegalArgumentException.class, () -> rectangle.calculateArea(-4, -5));
+        Rectangle rectangle = new Rectangle(-4, -5);
+        assertThrows(IllegalArgumentException.class, () -> rectangle.calculateArea());
     }
 
     @Test
     public void testAreaForZeroLength(){
-        Rectangle rectangle = new Rectangle();
-        assertEquals(0, rectangle.calculateArea(0, 5));
+        Rectangle rectangle = new Rectangle(0, 5);
+        assertEquals(0, rectangle.calculateArea());
     }
 
     @Test
     public void testAreaForZeroWidth(){
-        Rectangle rectangle = new Rectangle();
-        assertEquals(0, rectangle.calculateArea(4, 0));
+        Rectangle rectangle = new Rectangle(4, 0);
+        assertEquals(0, rectangle.calculateArea());
+    }
+
+    @Test
+    public void testAreasForDecimalValues(){
+        Rectangle rectangle = new Rectangle(4.0, 5.0);
+        assertEquals(20.0, rectangle.calculateArea());
     }
 }
