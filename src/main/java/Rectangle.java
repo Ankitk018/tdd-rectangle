375ef20c2052e2d1f2b1d4762e6cfc888a3128fe
ankitk018 <ankit.kumar1@thoughtworks.com>
2024-04-29 15:03:12 +0530

    [manas/ankit] add feature to get area of the rectangle for negative values and zeros

--- src/main/java/Rectangle.java
+++ src/main/java/Rectangle.java
@@ -2,6 +2,9 @@ public class Rectangle {
 
 
     public double calculateArea(double length, double width) {
+        if(length<0 || width < 0){
+            throw new IllegalArgumentException("Length and Width must be non-negative values");
+        }
         return length*width;
     }
 }
--- src/test/java/TestAreaOfRectangle.java
+++ src/test/java/TestAreaOfRectangle.java
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class TestAreaOfRectangle {
 
@@ -14,5 +15,35 @@ public class TestAreaOfRectangle {
         Rectangle rectangle = new Rectangle();
         //act //assert
         assertEquals(20, rectangle.calculateArea(4,5));
+    }
+
+    @Test
+    public void testAreaForNegativeLength(){
+        Rectangle rectangle = new Rectangle();
+        assertThrows(IllegalArgumentException.class, () -> rectangle.calculateArea(-4, 5));
+    }
+
+    @Test
+    public void testAreaForNegativeWidth(){
+        Rectangle rectangle = new Rectangle();
+        assertThrows(IllegalArgumentException.class, () -> rectangle.calculateArea(4, -5));
+    }
+
+    @Test
+    public void testAreaForNegativeLengthAndWidth(){
+        Rectangle rectangle = new Rectangle();
+        assertThrows(IllegalArgumentException.class, () -> rectangle.calculateArea(-4, -5));
+    }
+
+    @Test
+    public void testAreaForZeroLength(){
+        Rectangle rectangle = new Rectangle();
+        assertEquals(0, rectangle.calculateArea(0, 5));
+    }
+
+    @Test
+    public void testAreaForZeroWidth(){
+        Rectangle rectangle = new Rectangle();
+        assertEquals(0, rectangle.calculateArea(4, 0));
     }
 }
