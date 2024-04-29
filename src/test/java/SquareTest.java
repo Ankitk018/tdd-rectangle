cb15e39db2f8da096150e4460d6694907cf51355
ankitk018 <ankit.kumar1@thoughtworks.com>
2024-04-29 16:28:05 +0530

    [manas/ankit] Add feature to calculate the area of the square, and provided test case for scenario where side is positive, negative and zero

--- src/test/java/SquareTest.java
+++ src/test/java/SquareTest.java
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class SquareTest {
 
@@ -9,6 +10,20 @@ public class SquareTest {
     public void shouldReturnArea16OfSquareOfSide4(){
 
         Square square = new Square(4);
+
         assertEquals(16, square.area());
+    }
+
+    @Test
+    public void shouldThrowAnIllegealArgumentExceptionForNegativeValueOfSide(){
+
+        assertThrows(IllegalArgumentException.class, () -> new Square(-4) );
+    }
+
+    @Test
+
+    public void shouldThrowAnIllegealArgumentExceptionForZeroValueOfSide(){
+
+        assertThrows(IllegalArgumentException.class, () -> new Square(0) );
     }
 }
